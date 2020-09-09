82f8b03620085b85e7557fa1b8ec62a9edd89264
wallgrenmegan <58442494+wallgrenmegan@users.noreply.github.com>
2020-09-08 21:00:06 -0500

    fonts2

--- p5/Unit1/fonts/sketch.js
+++ p5/Unit1/fonts/sketch.js
@@ -1,7 +1,23 @@
-function setup() {
+var f1, f2;
 
+function setup() {
+  createCanvas(800,800);
+
+f1 = loadFont ("assets/font3.ttf");
+f2 = loadFont ("assets/font1.ttf");
+
+textAlign (CENTER);
 }
 
 function draw() {
+  background(100);
+  
+  textFont(f1);
+  textSize(60);
+  text("aliens", width/2, height/2 - 100);
+
+  textFont(f2);
+  textSize(30);
+  text("Very Cool", width/2, height/2 + 100);
 
 }

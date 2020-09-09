85d7be1abed19350e279c14557ce3c77a5fdede9
wallgrenmegan <58442494+wallgrenmegan@users.noreply.github.com>
2020-09-08 21:07:42 -0500

    did it work

--- p5/Unit1/fonts/sketch.js
+++ p5/Unit1/fonts/sketch.js
@@ -3,21 +3,21 @@ var f1, f2;
 function setup() {
   createCanvas(800,800);
 
-f1 = loadFont ("assets/font3.ttf");
-f2 = loadFont ("assets/font1.ttf");
+f1 = loadFont ("assets/Alien.ttf");
+f2 = loadFont ("assets/CloisterBlack.ttf");
 
 textAlign (CENTER);
 }
 
 function draw() {
   background(100);
-  
+
   textFont(f1);
-  textSize(60);
+  textSize(150);
   text("aliens", width/2, height/2 - 100);
 
   textFont(f2);
-  textSize(30);
+  textSize(72);
   text("Very Cool", width/2, height/2 + 100);
 
 }

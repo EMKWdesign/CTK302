b5e1111f4352ee640fa61b5a662fff1b11d3acd3
wallgrenmegan <58442494+wallgrenmegan@users.noreply.github.com>
2020-09-09 15:09:30 -0500

    Cowboye

--- p5/Unit1/mp1/sketch.js
+++ p5/Unit1/mp1/sketch.js
@@ -1,10 +1,141 @@
 function setup() {
-  createCanvas(500, 500);
+  // Sets the screen to be 720 pixels wide and 400 pixels high
+  createCanvas(700, 1000);
+}
+function draw(){
+
+  //Background
+  background('lightBlue');
+  noStroke();
+
+
+  //body
+  fill('white');
+rect(225, 300, 250, 250);
+
+
+  //head
+fill(255);
+ellipse(350, 300, 250, 250);
+
+
+    //pants
+  fill('midnightBlue');
+rect(225, 450, 250, 125);
+  fill('midnightBlue');
+rect(225, 550, 70, 300);
+    fill('midnightBlue');
+rect(405, 550, 70, 300);
+
+      //leg circle
+fill('lightBlue');
+ellipse(350, 575, 110, 110);
+
+
+
+      //Shoe details
+
+  fill('sienna');
+rect(225, 840, 70, 80);
+
+fill('sienna');
+rect(405, 840, 70, 80);
+
+  fill('sienna');
+ellipse(230, 840, 65, 65);
+
+    fill('sienna');
+ellipse(290, 840, 65, 65);
+
+fill('sienna');
+ellipse(410, 840, 65, 65);
+
+fill('sienna');
+ellipse(470, 840, 65, 65);
+
+  //leg boxes
+    fill('lightBlue');
+rect(295, 750, 110, 170);
+
+   fill('lightBlue');
+rect(0, 750, 225, 170);
+
+     fill('lightBlue');
+rect(475, 750, 225, 170);
+
+   //leg boxes
+    fill('darkOliveGreen');
+rect(295, 900, 110, 90);
+
+   fill('darkOliveGreen');
+rect(0, 900, 225, 90);
+
+     fill('darkOliveGreen');
+rect(475, 900, 225, 90);
+
+  //Shoes
+  fill('sienna');
+ellipse(225, 925, 110, 110);
+
+fill('sienna');
+ellipse(405, 925, 110, 110);
+
+   //groundfront
+    fill('oliveDrab');
+rect(0, 920, 700, 80);
+
+  //soles
+
+    fill('sienna');
+rect(170, 920, 125, 10);
+
+fill('sienna');
+rect(350, 920, 125, 10);
+
+  fill('oliveDrab');
+  triangle(225, 930, 260, 920, 260, 930);
+    fill('oliveDrab');
+  triangle(405, 930, 440, 920, 440, 930);
+
+  //cowboye hat
+  fill('sienna');
+rect(155, 190, 390, 50);
+    fill('sienna');
+ellipse(155, 190, 100, 100);
+    fill('lightBlue');
+ellipse(205, 140, 110, 100);
+    fill('sienna');
+ellipse(545, 190, 100, 100);
+    fill('lightBlue');
+ellipse(495, 140, 110, 100);
+
+   fill('sienna');
+ellipse(300, 170, 150, 200);
+   fill('sienna');
+ellipse(400, 170, 150, 200);
+   fill('sienna');
+rect(225, 170, 250, 50);
+   fill('white');
+rect(225, 240, 250, 50);
+
+
+  if (mouseIsPressed){
+   //eyez
+  fill('black');
+rect(235, 300, 50, 10);
+   fill('black');
+rect(335, 300, 50, 10);
+}
+else {
+    //eyez
+  fill('black');
+ellipse(260, 300, 50, 50);
+   fill('black');
+ellipse(360, 300, 50, 50);
 }
 
-function draw() {
-  ellipse(100, 100, 100, 100) ;
+
+
+fill('white');
+text(mouseX + ',' + mouseY, 20, 20);
 }
-//leave background at the top
-if (mouseIsPressed) {background('red';)}
-else {background (100);}
---
+++ p5/Unit1/mp1b/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  ellipse(100, 100, 100, 100) ;
+}
+//leave background at the top
+if (mouseIsPressed) {background('red';)}
+else {background (100);}

b1763d215ce8dc1e70d778c851b778ab684ee209
wallgrenmegan <58442494+wallgrenmegan@users.noreply.github.com>
2020-10-03 15:40:44 -0500

    AHHHHH

--- p5/Unit2/touches/sketch.js
+++ p5/Unit2/touches/sketch.js
@@ -17,39 +17,32 @@ function draw() {
 
   switch(numberOfTouches) {
     case 0:
-      background:('white');
+      background('white');
       text("If you can read this, \nDo me a favor and touch the screen.", 20, 80) ;
       break ;
 
       case 1:
-      push();
-      background:('lightBlue');
+      background('lightBlue');
       text("Haha nice! That tickles. \nDo it again. \n(Add a finger)", 20, 80) ;
-      pop();
+
       // put a picture here
       break ;
 
       case 2:
-        push();
-      background:('pink');
+      background('pink');
       text("Woah! Hahaha! \nMore! More! Hahaha! \n(Add a finger)", 20, 80) ;
-      pop();
             // put a picture here
       break ;
 
       case 3:
-        push();
-      background:('lavender');
+      background('lavender');
      text("HAHAHAHA! Oh my gosh- I can barely breathe \n It tickles so much! HAHA- \n(Add a finger)", 20, 80) ;
-     pop();
             // put a picture here
       break ;
 
       case 4:
-        push();
-      background:('moccasin');
+      background('moccasin');
       text("HeeHeeHaHA! HeHeHeHo! AhhahaHeHe! \nHohohohoh! HeeHeehoo! Haha~! \n(Add a finger)", 20, 80) ;
-      pop();
             // put a picture here
       break ;
 

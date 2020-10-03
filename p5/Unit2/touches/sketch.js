b2f2a31ae06b0a628a366ac8ab53da75cf8776f3
wallgrenmegan <58442494+wallgrenmegan@users.noreply.github.com>
2020-10-03 15:26:27 -0500

    pixel giggles

--- p5/Unit2/touches/sketch.js
+++ p5/Unit2/touches/sketch.js
@@ -1,36 +1,47 @@
 let numberOfTouches ;
+let myFont;
+function preload() {
+  myFont = loadFont('assets/dogicapixel.ttf');
+}
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  textFont(myFont);
+ textSize(20);
 }
 
 function draw() {
   clear();
   numberOfTouches = touches.length; //automatically available to us
-  text(numberOfTouches + ' touches', 5, 10);
+  text(numberOfTouches + ' touches', 20, 40);
 
   switch(numberOfTouches) {
     case 0:
-      text("If you can read this, \ndo me a favor and touch the screen", 5, 22) ;
+      background:('white');
+      text("If you can read this, \nDo me a favor and touch the screen.", 20, 80) ;
       break ;
 
       case 1:
-       text("Haha nice! That tickles. \ndo it again.", 5, 22) ;
+      background:('lightBlue');
+      text("Haha nice! That tickles. \nDo it again. \n(Add a finger)", 20, 80) ;
       // put a picture here
       break ;
 
       case 2:
-      text("Woah! Hahaha! \nMore! More! Hahaha!", 5, 22) ;
+      background:('pink');
+      text("Woah! Hahaha! \nMore! More! Hahaha! \n(Add a finger)", 20, 80) ;
             // put a picture here
       break ;
 
       case 3:
-     text("HAHAHAHA! Oh my gosh- I can barely breathe \n It tickles so much! HAHA-", 5, 22) ;
+      background:('lavender');
+     text("HAHAHAHA! Oh my gosh- I can barely breathe \n It tickles so much! HAHA- \n(Add a finger)", 20, 80) ;
             // put a picture here
       break ;
 
       case 4:
-      text("HeeHeeHaHA! HeHeHeHo! AhhahaHeHe! \nHohohohoh! HeeHeehoo! Haha~!", 5, 22) ; 
+      background:('moccasin');
+      text("HeeHeeHaHA! HeHeHeHo! AhhahaHeHe! \nHohohohoh! HeeHeehoo! Haha~! \n(Add a finger)", 20, 80) ;
             // put a picture here
       break ;
 

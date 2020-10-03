96edeffe11d2607a48c3074f834e4656c9512181
wallgrenmegan <58442494+wallgrenmegan@users.noreply.github.com>
2020-10-03 15:38:05 -0500

    pushpop

--- p5/Unit2/touches/sketch.js
+++ p5/Unit2/touches/sketch.js
@@ -22,26 +22,34 @@ function draw() {
       break ;
 
       case 1:
+      push();
       background:('lightBlue');
       text("Haha nice! That tickles. \nDo it again. \n(Add a finger)", 20, 80) ;
+      pop();
       // put a picture here
       break ;
 
       case 2:
+        push();
       background:('pink');
       text("Woah! Hahaha! \nMore! More! Hahaha! \n(Add a finger)", 20, 80) ;
+      pop();
             // put a picture here
       break ;
 
       case 3:
+        push();
       background:('lavender');
      text("HAHAHAHA! Oh my gosh- I can barely breathe \n It tickles so much! HAHA- \n(Add a finger)", 20, 80) ;
+     pop();
             // put a picture here
       break ;
 
       case 4:
+        push();
       background:('moccasin');
       text("HeeHeeHaHA! HeHeHeHo! AhhahaHeHe! \nHohohohoh! HeeHeehoo! Haha~! \n(Add a finger)", 20, 80) ;
+      pop();
             // put a picture here
       break ;
 

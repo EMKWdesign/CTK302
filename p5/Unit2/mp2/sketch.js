712b4b8e68ff26c8678dd618911e116cc9f18c6f
wallgrenmegan <58442494+wallgrenmegan@users.noreply.github.com>
2020-10-04 15:41:48 -0500

    C1 and C2

---
+++ p5/Unit2/mp2/sketch.js
@@ -0,0 +1,46 @@
+var mic;
+var vol;
+var c = 'lightBlue';
+let x = 0;
+let img;
+function preload() {
+  img = loadImage('assets/fish.png');
+}
+
+function setup() {
+  createCanvas(windowHeight, windowWidth);
+
+  // code for initializing mic in.
+  mic = new p5.AudioIn();
+  mic.start();
+  // Top-left corner of the img is at (10, 10)
+  // Width and height are 50 x 50
+}
+
+function draw() {
+  background(c);
+
+  // get the sound input
+  vol = (mic.getLevel()).toFixed(2); // returned level is between 0 and 1
+
+  // check how loud the input is
+  if (vol > .02) { // if the volume is LOUD?
+// Do something
+    x = x + 1;
+  }
+  image(img, x, 200, 201, 129.5);
+
+  // extra stuff for debugging
+  textSize(18);
+  text("Click the screen \n& make some noise! \nMy volume is " + vol, 10, 60);
+
+
+}
+
+
+// you need this code for audio programs and also, the user
+// needs to click the screen before the mic input will work.
+
+function touchStarted() {
+  getAudioContext().resume();
+}

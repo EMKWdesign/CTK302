9cb5dd86098d378612242056fbc254597c91f56e
wallgrenmegan <58442494+wallgrenmegan@users.noreply.github.com>
2020-10-19 17:35:48 -0500

    Pushed

---
+++ p5/Unit3/part2/sketch.js
@@ -0,0 +1,50 @@
+let cars = [];
+
+function setup() {
+  createCanvas(500, 500);
+
+  // for (let i = 0; i < 20; i++) {
+  //   cars.push(new Car());
+  // }
+}
+
+function draw() {
+  background('grey');
+  cars.push(new Car());
+
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].display();
+    cars[i].move();
+    if (car[i].a <= 0) {
+      cars.splice(i, 1);
+    }
+  }
+}
+
+class Car {
+
+  constructor() {
+    //attributes
+    this.pos = createVector(100, 100);
+    this.vel = createVector(random(-10), random(10));
+    this.r = random(255);
+    this.g = random(255);
+    this.b = random(255);
+    this.a = random(255);
+    this.s = random(10, 20);
+  }
+  //methods
+  display() {
+    fill(this.r, this.g, this.b, this.a, this.s);
+    ellipse(this.pos.x, this.pos.y, 20);
+  }
+
+  move() {
+    this.pos.add(this.vel);
+    this.a = this.a - 5;
+
+  }
+
+
+
+}
